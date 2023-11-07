e96ef0df2f9b011b48563a273d448ff542a30045
Nick S <oart4k@gmail.com>
2023-11-07 12:46:05 -0500

    tree-sitter-textobjects

--- lua/oneart/plugins/treesitter.lua
+++ lua/oneart/plugins/treesitter.lua
@@ -1,3 +1,87 @@
 return {
-	"nvim-treesitter/nvim-treesitter"
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+  dependancy = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-refactor",
+    -- "nvim-treesitter/nvim-treesitter-rainbow",
+  },
+      nvim-treesitter/nvim-treesitter-textobjects
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"css",
+				"dockerfile",
+				"go",
+				"gomod",
+				"html",
+				"java",
+				"javascript",
+				"json",
+				"jsonc",
+				"lua",
+				"python",
+				"regex",
+				"rust",
+				"toml",
+				"typescript",
+				"yaml",
+				"svelte",
+				"gitignore",
+				"markdown",
+				"markdown_inline",
+			},
+			highlight = {
+				enable = true,
+				disable = {},
+			},
+			indent = {
+				enable = true,
+				disable = {},
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			refactor = {
+				highlight_definitions = {
+					enable = true,
+					disable = {},
+				},
+				highlight_current_scope = {
+					enable = true,
+					disable = {},
+				},
+				smart_rename = {
+					enable = true,
+					disable = {},
+				},
+				navigation = {
+					enable = true,
+					disable = {},
+				},
+			},
+			textobjects = {
+				enable = true,
+				disable = {},
+			},
+			playground = {
+				enable = true,
+				disable = {},
+			},
+			rainbow = {
+				enable = true,
+				disable = {},
+			},
+		})
+	end,
 }
