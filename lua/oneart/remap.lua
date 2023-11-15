4ad404571de9acfc80e00ef23abe2239a361db99
Nick S <oart4k@gmail.com>
2023-11-15 16:00:59 -0500

    more plugins better

--- lua/oneart/remap.lua
+++ lua/oneart/remap.lua
@@ -22,3 +22,5 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+vim.keymap.set("v", "<leader>jq", ":'<,'>!jq", { desc = "Run on selected lines JQ cmd" })
