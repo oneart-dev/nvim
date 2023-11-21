8c0df8b35abd82d4d4115f685828dcc2963d1299
Nick S <oart4k@gmail.com>
2023-11-21 11:29:44 -0500

    better test config

--- lua/oneart/plugins/vim-test.lua
+++ lua/oneart/plugins/vim-test.lua
@@ -1,5 +1,18 @@
 return {
 	"vim-test/vim-test",
+	cmd = { "TestNearest", "TestFile", "TestSuite", "TestLast", "TestVisit" },
+	config = function()
+		vim.g["test#go#gotest#options"] = "-v -timeout 15s"
+
+		-- vim.g['test#strategy'] = 'neoterm'
+		-- vim.g['test#neoterm#term_position'] = 'vert'
+		vim.g["test#preserve_screen"] = 1
+		-- vim.g['test#javascript#runner'] = 'vitest'
+		vim.g.neoterm_shell = "zsh"
+		-- vim.g.neoterm_automap_keys = ''
+		vim.g.neoterm_default_mod = "vertical"
+		vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+	end,
 	keys = {
 		{ "<leader>tt", ":TestNearest<CR>" },
 		{ "<leader>tf", ":TestFile<CR>" },
