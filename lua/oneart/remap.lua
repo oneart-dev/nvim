a266bbee77b965d9e28616c318949924e7bd8955
Nick S <oart4k@gmail.com>
2023-11-30 17:19:59 -0500

    feature: add surround plugin; add scratchpad with cmd

--- lua/oneart/remap.lua
+++ lua/oneart/remap.lua
@@ -1,28 +1,27 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
---local builtin = require('telescope.builtin')
---vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
---vim.keymap.set('n', '<C-p>', builtin.git_files, {})
---vim.keymap.set('n', '<leader>fs', function()
---	builtin.grep_string({ search = vim.fn.input("Grep >") });
--- --end)
-
 vim.keymap.set("v", "p", '"_dP', { desc = "Copy without overwriting clipboard" }) -- copy without overwriting clipboard
 
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close" })
 
--- increment/decrement numbers
 vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
--- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
+vim.keymap.set("n", "leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 vim.keymap.set("v", "<leader>jq", ":'<,'>!jq", { desc = "Run on selected lines JQ cmd" })
 
 vim.keymap.set("n", "<leader>F", "gggqG", { desc = "format file" }) -- format file
+
+vim.keymap.set("n", "x", '"_x', { desc = "Delete without overwriting clipboard" }) -- delete without overwriting clipboard
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without overwriting clipboard" }) -- delete without overwriting clipboard
+vim.keymap.set({ "n", "v" }, "dd", '"_dd', { desc = "Delete without overwriting clipboard" }) -- delete without overwriting clipboard
+vim.keymap.set("n", "D", '"_D', { desc = "Delete without overwriting clipboard" }) -- delete without overwriting clipboard
+
+vim.keymap.set("n", "<leader>md", "vip:'<,'>!prettier --parser markdown<cr>", { desc = "format markdown" })
+vim.keymap.set("v", "<leader>md", ":'<,'>!prettier --parser markdown<cr>", { desc = "format markdown" })
---
+++ scratchpad/no-neck-pain-right.md
@@ -0,0 +1,36 @@
+| Cmd           | Desc           |
+| ------------- | -------------- |
+| <leader>md    | prettier md    |
+| gcc           | comment        |
+| ------------- | -------------- |
+| `ysiw)`       | surround       |
+| `ys$"`        | make strs      |
+| `ds]`         | del around     |
+| `dst`         | rm HTML        |
+| `cs'"`        | chg quotes     |
+| `cstp<CR>`    | <p>tag</p>     |
+| `dsf`         | func calls     |
+| `<leader>sv`  | split vert     |
+| `<leader>sh`  | split hor      |
+| `<leader>se`  | eq win         |
+| `<leader>sx`  | close win      |
+| `<leader>jq`  | json fmt       |
+| `<leader>F`   | format         |
+| `<leader>+`   | increment      |
+| `<leader>-`   | decrement      |
+| ------------- | ------------   |
+| `gR`          | Show refs      |
+| `gD`          | Go to decl     |
+| `gd`          | Show defs      |
+| `gi`          | Show impls     |
+| `gt`          | Show type defs |
+| `<leader>ca`  | Code actions   |
+| `<leader>rn`  | Smart rename   |
+| `<leader>D`   | Buffer diags   |
+| `<leader>d`   | Line diags     |
+| `[d`          | Prev diag      |
+| `]d`          | Next diag      |
+| `K`           | Show doc       |
+| `<leader>rs`  | Restart LSP    |
+
+
